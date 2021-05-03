ce8b8505f69e3b18f1599e1f44d3ba53b49c55f0
SeunghyunWoo <seunghyunwoo99@gmail.com>
2021-05-04 02:10:31 +0900

    prettier, eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    semi: ['error', 'never'],
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': 'error',
+  },
+}
