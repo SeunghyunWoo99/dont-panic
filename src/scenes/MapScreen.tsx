ac332373a911df02e1a0418a1f23504a93833d3e
SeunghyunWoo <seunghyunwoo99@gmail.com>
2021-05-12 02:10:55 +0900

    페이지에 지도 띄우기

--- src/scenes/MapScreen.tsx
+++ src/scenes/MapScreen.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 export default function MapScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Map</Text>
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{ latitude: 37.5326, longitude: 127.024612, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
+      />
     </View>
   )
 }
