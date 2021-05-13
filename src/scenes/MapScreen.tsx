77880038cc914a505fd94e843eaa019de8a3e7e8
SeunghyunWoo <seunghyunwoo99@gmail.com>
2021-05-14 02:03:28 +0900

    첫 랜더에 지도 영역 현재 위치로 표시

--- src/scenes/MapScreen.tsx
+++ src/scenes/MapScreen.tsx
@@ -1,15 +1,43 @@
-import * as React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { View } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps'
+import Geolocation from 'react-native-geolocation-service'
 
 export default function MapScreen() {
+  const [region, setRegion] = useState<Region>({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.02,
+  })
+
+  const setLocationFromCurrent = useCallback(
+    () =>
+      Geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords
+          setRegion({
+            latitude,
+            longitude,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.02,
+          })
+        },
+        (error) => {
+          console.error(error)
+        },
+        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+      ),
+    [],
+  )
+
+  useEffect(() => {
+    setLocationFromCurrent()
+  }, [setLocationFromCurrent])
+
   return (
     <View style={{ flex: 1 }}>
-      <MapView
-        style={{ flex: 1 }}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{ latitude: 37.5326, longitude: 127.024612, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
-      />
+      <MapView style={{ flex: 1 }} provider={PROVIDER_GOOGLE} region={region} />
     </View>
   )
 }
