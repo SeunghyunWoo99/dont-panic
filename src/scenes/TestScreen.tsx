11c5d10b72907eda6d6509bd4df1e25f89e2f3b8
SeunghyunWoo <seunghyunwoo99@gmail.com>
2021-05-17 22:30:14 +0900

    코로나 테스트 UI 뼈대: horizontal scrollview snap 추가

--- src/scenes/TestScreen.tsx
+++ src/scenes/TestScreen.tsx
@@ -1,10 +1,52 @@
-import * as React from 'react'
-import { Text, View } from 'react-native'
+import React, { useRef } from 'react'
+import { View, ScrollView } from 'react-native'
+import { size } from 'utils'
+
+const CARD_WIDTH = size.screenWidth
+const CARD_HEIGHT = size.screenHeight
 
 export default function TestScreen() {
+  const scrollViewRef = useRef<ScrollView>(null)
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Tests</Text>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        ref={scrollViewRef}
+        showsHorizontalScrollIndicator={false}
+        decelerationRate={0.9}
+        snapToInterval={CARD_WIDTH}
+        snapToAlignment="center">
+        <View
+          style={{
+            width: CARD_WIDTH,
+            height: CARD_HEIGHT,
+            backgroundColor: 'red',
+          }}
+        />
+        <View
+          style={{
+            width: CARD_WIDTH,
+            height: CARD_HEIGHT,
+            backgroundColor: 'green',
+          }}
+        />
+        <View
+          style={{
+            width: CARD_WIDTH,
+            height: CARD_HEIGHT,
+            backgroundColor: 'blue',
+          }}
+        />
+        <View
+          style={{
+            width: CARD_WIDTH,
+            height: CARD_HEIGHT,
+            backgroundColor: 'purple',
+          }}
+        />
+      </ScrollView>
     </View>
   )
 }
