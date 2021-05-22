8a6e01c5e4c084b2cea32a1ad9823eb3ff0b8904
Seunghyun Woo <seung-hyun.woo@mecute.com>
2021-05-22 16:33:11 +0900

    임시 커밋

--- src/scenes/ResultScreen.tsx
+++ src/scenes/ResultScreen.tsx
@@ -1,10 +1,32 @@
-import * as React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Text, View } from 'react-native'
+import LottieView from 'lottie-react-native'
+import AnimatedLottieView from 'lottie-react-native'
+import { scale } from 'react-native-size-matters'
+import { transform } from '@babel/core'
 
 export default function ResultScreen(props: { route: { params: { score: number } } }) {
+  const lottieRef = useRef<AnimatedLottieView>(null)
+  const { score } = props.route.params
+
+  // useEffect(() => {
+  //   // lottieRef.current?.play(71, 71)
+  //   lottieRef.current?.play(0, Math.floor((71 * score) / 100))
+  // }, [score])
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{props.route.params.score}</Text>
+      <View style={{ transform: [{ rotate: '-90deg' }] }}>
+        <LottieView
+          ref={lottieRef}
+          autoPlay
+          // loop={false}
+          speed={2}
+          style={{ width: scale(580) }}
+          source={require('lotties/wave.json')}
+        />
+      </View>
+      <Text style={{ position: 'absolute', fontSize: scale(64), fontWeight: 'bold' }}>{props.route.params.score}</Text>
     </View>
   )
 }
