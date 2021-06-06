954c70ec0825650d45ce5e0a917f9603aac3dd84
SeunghyunWoo <seunghyunwoo99@gmail.com>
2021-06-07 04:33:32 +0900

    앱 메인 컬러 수정: #3F296F -> #2F6D43

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -9,8 +9,8 @@ export default {
     hint: '#bbbbbb',
     /** #ffffff white: ![](https://www.colorhexa.com/ffffff.png) */
     button: '#ffffff',
-    /** #3F296F deepPurple: ![](https://www.colorhexa.com/a26077.png) */
-    main: '#3F296F',
+    /** #2F6D43 deepGreen: ![](https://www.colorhexa.com/2F6D43.png) */
+    main: '#2F6D43',
     /** #ff5e73 coralPink: ![](https://www.colorhexa.com/ff5e73.png) */
     caution: '#ff5e73',
   },
@@ -20,11 +20,13 @@ export default {
 
   /** 버튼 색상 */
   button: {
-    /** #3F296F deepPurple: ![](https://www.colorhexa.com/3F296F.png) */
-    primary: '#3F296F',
+    /** #2F6D43 deepGreen: ![](https://www.colorhexa.com/2F6D43.png) */
+    primary: '#2F6D43',
+    /** #ffffff white: ![](https://www.colorhexa.com/ffffff.png) */
+    secondary: '#ffffff',
     disabled: {
-      /** #d0ccd9 fadedPurple: ![](https://www.colorhexa.com/d0ccd9.png) */
-      primary: '#d0ccd9',
+      /** #d8e3df fadedPurple: ![](https://www.colorhexa.com/d8e3df.png) */
+      primary: '#d8e3df',
     },
   },
 
@@ -34,16 +36,16 @@ export default {
     primary: '#ffffff',
     /** #f5f5f5 white: ![](https://www.colorhexa.com/f5f5f5.png) */
     secondary: '#f5f5f5',
-    /** #3F296F deepPurple: ![](https://www.colorhexa.com/a26077.png) */
-    main: '#3F296F',
+    /** #2F6D43 deepGreen: ![](https://www.colorhexa.com/2F6D43.png) */
+    main: '#2F6D43',
   },
 
   /** 기타 색상들 */
   palette: {
-    /** 앱 메인 색상(딥퍼플) ![](https://www.colorhexa.com/3F296F.png) */
-    main: '#3F296F',
-    /** disabled 버튼 색상 ![](https://www.colorhexa.com/d0ccd9.png) */
-    disabled: '#d0ccd9',
+    /** 앱 메인 색상(딥그린) ![](https://www.colorhexa.com/2F6D43.png) */
+    main: '#2F6D43',
+    /** disabled 버튼 색상 ![](https://www.colorhexa.com/d8e3df.png) */
+    disabled: '#d8e3df',
     /** 아이콘 등에서 자주 사용되는 색상(쨍한 분홍색) ![](https://www.colorhexa.com/ff5e73.png) */
     mainPink: '#ff5e73',
     /** 토스트, 버튼 등에서 사용되는 파란색 */
