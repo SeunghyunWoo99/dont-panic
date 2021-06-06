788f0d614be9ae6a4e3aa7e179e8fb8b63d0b570
SeunghyunWoo <seunghyunwoo99@gmail.com>
2021-06-07 04:03:27 +0900

    하단 TabBar 아이콘 설정 및 svg 에셋 추가

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,4 @@
+export { default as Map } from './map.svg'
+export { default as MapActive } from './mapActive.svg'
+export { default as News } from './news.svg'
+export { default as NewsActive } from './newsActive.svg'
